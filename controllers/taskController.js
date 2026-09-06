b8e65621571c88a478b05b3947213d0cb40e1fad
tks <t.sauntoo@alustudent.com>
2026-09-06 14:53:13 +0400

    bug fix. Function was missing

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -65,24 +65,35 @@ const taskController = {
 
         const taskId = req.params.id;
 
-        //if there is no error 
-        if (!error) {
+        task.getTaskById(taskId, (error, results) => {
+             //if there is no error 
+            if (!error) {
 
-            //if no results found, no result sent
-            if (results.length === 0) {
-                return res.status(404).json({
-                    message: "Task not found"
-                });
+                //if no results found, no result sent
+                if (results.length === 0) {
+                    return res.status(404).json({
+                        message: "Task not found"
+                    });
+                } else {
+                    //return first task found 
+                    return res.status(200).json(results[0]);
+                }
+
             } else {
-                //return first task found 
-                return res.status(200).json(results[0]);
-            }
+                return res.status(500).json({
+                    message: "Database error"
+                });
+            };
 
-        } else {
-            return res.status(500).json({
-                message: "Database error"
-            });
-        };
+
+        })
+
+       
+    },
+
+    updateTask: (res, req) => {
+
+        
 
     }
 
